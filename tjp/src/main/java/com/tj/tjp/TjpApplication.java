1756671d2fa35fa82a9906f73c0a7e3bcc49bbf4
chlorella71 <chlorella71@naver.com>
2025-07-09 10:57:17 +0900

    init: springboot

---
+++ tjp/src/main/java/com/tj/tjp/TjpApplication.java
@@ -0,0 +1,14 @@
+package com.tj.tjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjpApplication.class, args);
+		System.out.print("Hello tj!");
+	}
+
+}
